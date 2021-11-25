2b0ee62927a0368725bdefc0bd2527df4c8f2c75
JoakoV3 <joaquinvarelaivan@gmail.com>
2021-11-24 22:23:29 -0300

    making a new build

---
+++ precache-manifest.612ba1dc3e5dd282752a0d6ecd31f6fa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c934e8f3281d115faa844b5975ab637",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d5ce7546a0605080135",
+    "url": "./static/css/2.37b96200.chunk.css"
+  },
+  {
+    "revision": "984b53642b7840e22dae",
+    "url": "./static/css/main.a4eb7d16.chunk.css"
+  },
+  {
+    "revision": "6d5ce7546a0605080135",
+    "url": "./static/js/2.055d3992.chunk.js"
+  },
+  {
+    "revision": "166030ca0d325db22f5b583c1e540220",
+    "url": "./static/js/2.055d3992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984b53642b7840e22dae",
+    "url": "./static/js/main.db2160f7.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "2260eb1a0f48eb82d60fd7493339a9f7",
+    "url": "./static/media/Imagen3.2260eb1a.png"
+  },
+  {
+    "revision": "44388840b9868f3daed433b195eaf33e",
+    "url": "./static/media/Imagen5.44388840.png"
+  },
+  {
+    "revision": "9188df2a22509d52e5ee2b3db80d2f30",
+    "url": "./static/media/Imagen6.9188df2a.png"
+  },
+  {
+    "revision": "db7933e6a7f7acaf99a33f39b286974b",
+    "url": "./static/media/Javascript.db7933e6.png"
+  },
+  {
+    "revision": "1ea234e3eaee0ec4058caa0b5791ea6a",
+    "url": "./static/media/RickMorty.1ea234e3.png"
+  },
+  {
+    "revision": "9d574da0b032b5bebf49ea6860f3b8c2",
+    "url": "./static/media/Turnos.9d574da0.png"
+  },
+  {
+    "revision": "a5b3879f4b6c62bd4edbf569e2a5019b",
+    "url": "./static/media/hero-image.a5b3879f.jpg"
+  },
+  {
+    "revision": "d3d76541eb4dc6bd39a4e11f2f163609",
+    "url": "./static/media/imagen7.d3d76541.png"
+  },
+  {
+    "revision": "967c1cd4e3a496c9334f81f54a27f901",
+    "url": "./static/media/joako-pic.967c1cd4.jpg"
+  },
+  {
+    "revision": "e86e466b0a1e1ae984296a3c3b7679a5",
+    "url": "./static/media/memotestPreview.e86e466b.png"
+  },
+  {
+    "revision": "25a7209ccc20fcf9ecdcd5ed8b80957b",
+    "url": "./static/media/seguridadPreview.25a7209c.png"
+  }
+]);
